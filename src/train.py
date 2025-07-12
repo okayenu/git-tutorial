d742cfe91b1730924389625961a06dc975e3c740
oke <okayenu@gmail.com>
2025-07-12 18:07:00 -0500

    small train.py fix, pls work

--- src/train.py
+++ src/train.py
@@ -204,3 +204,5 @@ def log_training_report(history, report_path: str = None):
 # [2025-06-29 5:19 PM] Task 31: Add callback to get_callbacks()
 
 # [2025-07-05 2:53 PM] Task 10: Set model.compile with appropriate optimizer
+
+# [2025-07-12 6:07 PM] Task 11: Train 4-block model with same config as baseline
